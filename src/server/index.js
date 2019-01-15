418fece39e2d23665ec9f38cf3c332dd9f91b7d9
thms <thmsdxtrk@gmail.com>
2019-01-15 03:19:39 -0500

    update src/server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -6,21 +6,20 @@ var path = require("path");
 var app = express();
 var port = process.env.PORT || 3001;
 //Static file declaration
-app.use(express.static(path.join(__dirname, "client/build")));
+console.log("dir is ---", path.resolve(__dirname, "../client/build"));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 //production mode
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.resolve(__dirname, "../client/build")));
     //
     app.get("*", function (req, res) {
-        res.sendfile(path.join((__dirname = "client/build/index.html")));
+        res.sendfile(path.resolve(__dirname, "../client/build/index.html"));
     });
 }
-else {
-    //build mode
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname + "/client/public/index.html"));
-    });
-}
+//build mode
+app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../client/public/index.html"));
+});
 //start server
 app.listen(port, function (req, res) {
     console.log("server listening on port: " + port);
