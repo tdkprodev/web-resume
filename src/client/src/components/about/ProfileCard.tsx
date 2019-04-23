7c402ee5b340bfb9af93c3ccfd3498346f5e4166
thms <thmsdxtrk@gmail.com>
2019-04-23 07:26:51 -0400

    dynamic greeting tooltip

--- src/client/src/components/about/ProfileCard.tsx
+++ src/client/src/components/about/ProfileCard.tsx
@@ -3,12 +3,52 @@ import * as React from "react";
 import { Tooltip, Typography } from "@material-ui/core";
 import myPhoto from "../../images/prophoto.png";
 
-class ProfileCard extends React.Component<{}> {
+const myGreetings = [
+  "Hello there!",
+  "Thanks for browsing.",
+  "I'm glad you're here.",
+  "Did you know birds fly south?"
+];
+
+interface IState {
+  greeting: string;
+  greetingIntervalId: number;
+}
+
+class ProfileCard extends React.Component<{}, IState> {
+  public state = {
+    greeting: myGreetings[0],
+    greetingIntervalId: 0
+  };
+
+  public componentDidMount() {
+    const rand = Math.floor(Math.random() * myGreetings.length);
+    this.setState({
+      greeting: myGreetings[rand]
+    });
+
+    const greetingIntervalId = window.setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * myGreetings.length);
+      this.setState({
+        greeting: myGreetings[randomIndex]
+      });
+    }, 5000);
+
+    this.setState(() => ({
+      greetingIntervalId
+    }));
+  }
+
+  public componentWillUnmount() {
+    window.clearInterval(this.state.greetingIntervalId);
+  }
+
   public render() {
+    const { greeting } = this.state;
     return (
       <div className="profile__description">
         <div className="profile__photo circle animateFromTop">
-          <Tooltip title="Hello there!" placement="right-start">
+          <Tooltip title={greeting} placement="right-start">
             <img className="image circle" src={myPhoto} alt="My Photo" />
           </Tooltip>
         </div>
