d92fcd3bd2c3ad7bb13e0edc9bf33645be8e0e19
thms <thmsdxtrk@gmail.com>
2019-05-02 22:37:32 -0400

    resolve app

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,6 +1,6 @@
+import App from "@src/app";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./app";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
